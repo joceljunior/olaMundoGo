a597b7ff4a93b89044dbe743f3bd94ca943c3158
Jocel <jocel.junior.silva@gmail.com>
2021-10-08 13:22:59 -0300

    go conventions.

--- hello.go
+++ hello.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, Jocel!")
+	var name string = "Jocel" // declarar tipagem depois do nome da variavel
+	//var idade int -- nao deixa declarar uma variavel e nao usar
+	var version float32 = 1.01
+	// go aceita o var com inferencia de tipo
+	// outra opção é :
+	idade := 27
+
+	// não usar ; ao final da instrução
+	fmt.Println("Hello World,", name)  // concatenar com a virgula
+	fmt.Println("Sua idade é,", idade) // concatenar com a virgula
+	fmt.Println("Versão do programa", version)
 }
